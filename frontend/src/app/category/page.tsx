a462bfd2531b5ff572b3a0ff7e7d3a87416ac5be
satsuralala <satsuraltumurbat@gmail.com>
2024-09-16 22:51:35 +0800

    size-remained

--- frontend/src/app/category/page.tsx
+++ frontend/src/app/category/page.tsx
@@ -5,6 +5,7 @@ import { Heart } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox"
+import CategoryList from "@/components/product/productList";
 
 export default function Category() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -66,36 +67,7 @@ export default function Category() {
 
       </div>
 
-      <div className="grid grid-cols-3 gap-x-5 gap-y-12 overflow-hidden ">
-        {products.slice(1).map((product, index) => (
-          <div
-            key={product._id}
-            
-          >
-            <div className="overflow-hidden relative  w-[244px] h-[331px] rounded-xl">
-              <Image
-                src={product.images}
-                alt={product.productName}
-                width={100}
-                height={100}
-                className="relative transition-transform duration-300 ease-in-out transform hover:scale-110 object-cover rounded-xl w-full"
-              />
-              <Heart
-                className="absolute top-2 right-2 z-10 cursor-pointer"
-                color={savedProducts.has(product._id) ? "black" : "gray"}
-                fill={savedProducts.has(product._id) ? "black" : "none"}
-                onClick={() => handleSaveClick(product._id)}
-              />
-            </div>
-            <p className="text-[#000000] text-base font-normal pt-2">
-              {product.description}
-            </p>
-            <p className="text-[#000000] text-base font-bold pt-1">
-              {product.price} ₮
-            </p>
-          </div>
-        ))}
-      </div>
+      <CategoryList/>
 
     </div>
       
