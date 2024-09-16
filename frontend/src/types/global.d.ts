65a0e74883b1079a1e0561c6b1e53db7a91b8cfb
satsuralala <satsuraltumurbat@gmail.com>
2024-09-16 14:51:51 +0800

    new

--- frontend/src/types/global.d.ts
+++ frontend/src/types/global.d.ts
@@ -1,1 +1,17 @@
 // TypeScript definitions for global types used throughout the application.
+interface Product {
+    _id: string;
+    size: string;
+    productName: string;
+    categoryId: string;
+    price: number;
+    qty: number;
+    thumbnails: string;
+    images: string;
+    coupon: string;
+    salePercent: string;
+    description: string;
+    viewsCount: number;
+    createdAt: string;
+    updatedAt: string;
+  }
