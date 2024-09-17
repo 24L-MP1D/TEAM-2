b5168c70f800fbdb5bc5b5912d63830792c0cf5e
Miigaa0204 <mytawochir@gmail.com>
2024-09-17 20:44:33 +0800

    update

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        default2:"bg-[#FFFFFF] py-2 px-16 rounded-[16px] text-sm font-semibold" 
       },
       size: {
         default: "h-9 px-4 py-2",
