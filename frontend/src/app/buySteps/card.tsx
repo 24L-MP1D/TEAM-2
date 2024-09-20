ff42d4b597eb7ce26a94f29db372b154b0266b71
Miigaa0204 <mytawochir@gmail.com>
2024-09-20 19:49:56 +0800

    buy steps cart finish

--- frontend/src/app/buySteps/card.tsx
+++ frontend/src/app/buySteps/card.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { PiTrashLight } from "react-icons/pi";
 
 export default function Card() {
+  var numeral = require("numeral");
+  
   const mockProducts = [
     {
       id: 1,
@@ -51,27 +53,42 @@ export default function Card() {
     setProducts(updatedProducts);
   };
 
+  const removePieces = (index: number) => {
+    const updatedProducts = [...products];
+    if (updatedProducts[index].pieces > 1) {
+      updatedProducts[index].pieces--;
+      setProducts(updatedProducts);
+    }
+  };
+
+  const removeProduct = (index: number) => {
+    const updatedProducts = products.filter((_, i) => i !== index);
+    setProducts(updatedProducts);
+  };
 
   return (
     <div className="bg-white p-8 rounded-2xl">
       <div className="w-[574px] flex flex-col gap-6">
         <div className="flex flex-col gap-4">
           <div className="font-bold text-black text-xl flex">
-            1.Сагс &nbsp;<div className="text-[#71717A]">({products.length})</div>
+            1.Сагс &nbsp;
+            <div className="text-[#71717A]">({products.length})</div>
           </div>
           {products.map((product, index) => (
             <div className="flex items-start gap-6 p-4 rounded-2xl border-[#ECEDF0] border-solid border-[1px]">
               <div className="w-[100px] h-[100px] rounded-2xl overflow-hidden">
                 {product.image}
               </div>
-              <div className="flex flex-1 flex-col gap-1">  
+              <div className="flex flex-1 flex-col gap-1">
                 <div className="text-base font-normal">{product.name} </div>
-                <div className="flex flex-col gap-2 ">  
+                <div className="flex flex-col gap-2 ">
                   <div>
                     {/* item iin shirhegiig nemj hasah function */}
                     <div className="flex items-center">
                       <Button
                         className="w-8 h-8 rounded-full border-solid border-[1px] border-black bg-transparent text-black text-lg hover:bg-slate-300"
+                        onClick={() => removePieces(index)}
+                        disabled={product.pieces < 2}
                       >
                         -
                       </Button>
@@ -85,18 +102,30 @@ export default function Card() {
                     </div>
                   </div>
                   <div className="font-bold text-base">
-                    {product.price}&#8366;
+                    {numeral(product.price * product.pieces).format("0,0")}
+                    &#8366;
                   </div>
                 </div>
               </div>
               <div className="w-10 h-10 flex justify-center items-center">
-                <PiTrashLight className="w-6 h-6" />
+                <PiTrashLight
+                  className="w-6 h-6"
+                  onClick={() => removeProduct(index)}
+                />
               </div>
             </div>
           ))}
           <div className="flex justify-between pb-6">
             <div className="text-lg font-normal">Нийт төлөх дүн:</div>
-            <div className="font-bold text-xl">360'000&#8366;</div>
+            <div className="font-bold text-xl">
+              {numeral(
+                products.reduce(
+                  (total, product) => total + product.price * product.pieces,
+                  0
+                )
+              ).format("0,0")}
+              &#8366;
+            </div>
           </div>
         </div>
         <div className=" h-[9] text-end">
