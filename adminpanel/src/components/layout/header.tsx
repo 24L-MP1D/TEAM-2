0ecf6c2d77467ba47563e6b1a34cb54222d487c3
Miigaa0204 <mytawochir@gmail.com>
2024-09-23 19:32:42 +0800

    left bar front

--- adminpanel/src/components/layout/header.tsx
+++ adminpanel/src/components/layout/header.tsx
@@ -1,35 +1,32 @@
-"use client"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+"use client";
 
+import Image from "next/image";
+import { useState } from "react";
+import { MdNotificationsNone } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa6";
 
 export default function Header() {
-    const [openSearch, setOpenSearch] = useState<boolean>(false);
-    const handleClick = (): void => {
-      setOpenSearch(true);
-    };
-  
-    return (
-      <div className="bg-black py-3 px-6 justify-between flex">
-       
-         
-                <Image src="/logo.png" alt="icon" width={30} height={30} className=""/>
-               
-           
-            
-           
-            
-            <div className="flex gap-6 items-center">
-             
-                  <div className="py-2 px-3  border-solid border-[1px] border-[#2563eb] rounded-[18px] font-normal text-sm bg-[#2563EB] hover:bg-opacity-70 text-white hover:text-opacity-50 ">
-                   Username
-                  </div>
-              
-            </div>
-            </div>
-        
-   
-    );
+  const [openSearch, setOpenSearch] = useState<boolean>(false);
+  const handleClick = (): void => {
+    setOpenSearch(true);
+  };
+
+  return (
+    <div className="bg-black">
+      <div className="py-1 px-6 flex justify-between items-center gap-4 max-w-[1440px] h-12 mx-auto">
+        <div className="flex justify-start items-center flex-1">
+          <Image src="/symbol.png" alt="icon" width={24} height={24} />
+        </div>
+        <div className="w-9 h-9 flex justify-center items-center">
+          <MdNotificationsNone className="w-5 h-5" />
+        </div>
+        <div className="flex items-center">
+          <div className="w-9 h-9 flex justify-center items-center">
+            <FaRegUser className="w-4 h-4" />
+          </div>
+          <div className="px-2 py-[6px] font-normal text-sm">Username</div>
+        </div>
+      </div>
+    </div>
+  );
 }
