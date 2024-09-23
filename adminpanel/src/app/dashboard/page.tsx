4e9cd6bc7f7c3e7a5ac042f17c01142f7ed2c598
Odonkhuu Munkhzul <odonkhuumunkhzul@Odonkhuus-MacBook-Pro.local>
2024-09-23 13:49:20 +0800

    new change at dashbaord

--- adminpanel/src/app/dashboard/page.tsx
+++ adminpanel/src/app/dashboard/page.tsx
@@ -8,113 +8,193 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
 
-export default function Dashboard() {
-  const [, setAdmin] = useState(false);
-  const [selectedFeature, setSelectedFeature] = useState<string>("adminPanel");
 
-  // const handleSelectedFeature = (e: { target: { value: any; }; }) => {
-  //   selectedFeature(e.target.value)
-  // }
-  const renderSelectedFeature = () => {
-    switch (selectedFeature) {
-      case "adminProduct": <AdminProducts /> ; break; 
-      case "adminSetting": <AdminSetting /> ; break;
-    }
-  }
+
+
+// export default function Dashboard() {
+//   const [, setAdmin] = useState(false);
+//   const [selectedFeature, setSelectedFeature] = useState<string>("adminPanel");
+
+//   // const handleSelectedFeature = (e: { target: { value: any; }; }) => {
+//   //   selectedFeature(e.target.value)
+//   // }
+//   const renderSelectedFeature = () => {
+//     switch (selectedFeature) {
+//       case "adminProduct": <AdminProducts /> ; break; 
+//       case "adminSetting": <AdminSetting /> ; break;
+//     }
+//   }
+//   return (
+//     <div className="flex flex mt-20 justify-center">
+
+//       <div className="flex flex-col bg-slate-200 w-[200px] ">
+//         {/* <Button variant="default" className="text-white" value={'adminPanel'} onClick={handleSelectedFeature('adminPanel')}>  */}
+//           Хяналтын самбар
+//         {/* </Button> */}
+//         <Button variant="default" className="text-white">
+//           Захиалга
+//         </Button>
+//         <Button variant="default" className="text-white">
+//           Орлого
+//         </Button>
+//         <Button variant="default" className="text-white" onClick={() => (true)}>
+//           Бүтээгдэхүүн
+//         </Button>
+//         <Button variant="default" className="text-white" onClick={() => setAdmin(true)}>
+//           Тохиргоо
+//         </Button>
+//       </div>
+//       <div className="flex flex-1 bg-slate-200  mr-20">
+//       {/* <renderSelectedFeature/> */}
+//       </div>
+//     </div>
+//   );
+// }
+
+
+export default function TabsDemo() {
   return (
-    <div className="flex flex mt-20 justify-center">
+    <div>
+      <Tabs defaultValue="account" className="w-[800px] mr-500 container ">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="account" >Бүтээгдэхүүн</TabsTrigger>
+          <TabsTrigger value="password">Ангилал</TabsTrigger>
+        </TabsList>
+        <Button variant="default" className="mt-2 ">+Бүтээгдэхүүн нэмэх </Button>
+        <div>
 
-      <div className="flex flex-col bg-slate-200 w-[200px] ">
-        {/* <Button variant="default" className="text-white" value={'adminPanel'} onClick={handleSelectedFeature('adminPanel')}>  */}
-          Хяналтын самбар
-        {/* </Button> */}
-        <Button variant="default" className="text-white">
-          Захиалга
-        </Button>
-        <Button variant="default" className="text-white">
-          Орлого
-        </Button>
-        <Button variant="default" className="text-white" onClick={() => (true)}>
-          Бүтээгдэхүүн
-        </Button>
-        <Button variant="default" className="text-white" onClick={() => setAdmin(true)}>
-          Тохиргоо
-        </Button>
-      </div>
-      <div className="flex flex-1 bg-slate-200  mr-20">
-      {/* <renderSelectedFeature/> */}
-      </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger className="my-5">Ангилал</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel className="ml-5 border border-1-gray">Ангилал</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+            </DropdownMenuContent>
+            <DropdownMenuTrigger className="ml-5 border border-1-gray">Үнэ</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel className="ml-5 border border-1-gray">Ангилал</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+            </DropdownMenuContent>
+            <DropdownMenuTrigger className="ml-5 border border-1-gray">Сараар</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel className="ml-5 border border-1-gray">Ангилал</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+
+        </div>
+        <TabsContent value="account">
+          <CardContent>
+            <CardHeader>
+              <CardTitle>Account</CardTitle>
+              <CardDescription>
+                Make changes to your account here. Click save when you're done.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="flex-1 ">
+                <table className="table-auto table-fixed border-2-gray border border-slate-400 border-slate-400">
+                  <thead className="">
+                    <tr className="border border-slate-400">
+                      <th className="w-[200px] h-[10px]">Бүтээгдэхүүн</th>
+                      <th className="w-[200px] h-[10px]">Aнгилал</th>
+                      <th className="w-[200px] h-[10px]">Yнэ</th>
+                      <th className="w-[200px] h-[10px]">Yлдэгдэл</th>
+                      <th className="w-[200px] h-[10px]">Зарагдсан</th>
+                      <th className="w-[200px] h-[10px]">Нэмсэн огноо</th>
+                    </tr>
+                  </thead>
+                  <tbody className="border-x-1 border border-slate-300">
+                    <tr className="border-x-1 border border-slate-300">
+                      <td className="w-[200px]">2343242355</td>
+                      <td className="w-[200px]">Malcolm Lockyer</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                    </tr>
+                    <tr>
+                      <td className="w-[200px]">2343242355</td>
+                      <td className="w-[200px]">Malcolm Lockyer</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                    </tr>
+                    <tr>
+                      <td className="w-[200px]">2343242355</td>
+                      <td className="w-[200px]">Malcolm Lockyer</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save changes</Button>
+            </CardFooter>
+          </CardContent >
+        </TabsContent>
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, you'll be logged out.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2 d">
+              <div className="flex-1 bg-slate-300 ">
+                <table className="table-auto table-fixed border border-slate-400 ">
+                  <thead className="">
+                    <tr className="border-x-1 border border-slate-300">
+                      <th className="w-[200px] h-[10px]">Бүтээгдэхүүн</th>
+                      <th className="w-[200px] h-[10px]">Aнгилал</th>
+                      <th className="w-[200px] h-[10px]">Yнэ</th>
+                      <th className="w-[200px] h-[10px]">Yлдэгдэл</th>
+                      <th className="w-[200px] h-[10px]">Зарагдсан</th>
+                      <th className="w-[200px] h-[10px]">Нэмсэн огноо</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="border-x-1 border border-slate-300">
+                      <td className="w-[200px] ">2343242355</td>
+                      <td className="w-[200px] ">Malcolm Lockyer</td>
+                      <td className="w-[200px] border border-x-1 border-slate-300">1961</td>
+                      <td className="w-[200px] border border-x-1 border-slate-300">1961</td>
+                      <td className="w-[200px] border border-x-1 border-slate-300">1961</td>
+                      <td className="w-[200px] border border-x-1 border-slate-300">1961</td>
+                    </tr>
+                    <tr className="border-x-1 border border-slate-300">
+                      <td className="w-[200px]">2343242355</td>
+                      <td className="w-[200px]">Malcolm Lockyer</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                      <td className="w-[200px] text-center">1961</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
-  );
-}
-
-// {user ? <UserInfo /> : <OrderHistory />}
-/* Left-Bar */
-
-// position: absolute;
-// width: 222px;
-// height: 976px;
-// left: 0px;
-// top: 48px;
-
-// background: #FFFFFF;
-
-
-
-export function TabsDemo() {
-  return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">
-        <CardContent>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </CardContent >
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
   )
 }
