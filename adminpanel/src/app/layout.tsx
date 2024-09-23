404bd89c432564b3ade88ea0b56239c71f21ce8b
satsuralala <satsuraltumurbat@gmail.com>
2024-09-23 10:11:30 +0800

    register-admin

--- adminpanel/src/app/layout.tsx
+++ adminpanel/src/app/layout.tsx
@@ -26,9 +26,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white` }
       >
+        <div >
         {children}
+
+        </div>
+
       </body>
     </html>
   );
