3e83d416cbb1ab5e4131a027e9c877129223b30d
satsuralala <satsuraltumurbat@gmail.com>
2024-09-25 12:30:26 +0800

    added categoty

--- adminpanel/src/app/addproduct/page.tsx
+++ adminpanel/src/app/addproduct/page.tsx
@@ -2,7 +2,13 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
-import { Check, ChevronLeft, ChevronsUpDown, Plus } from "lucide-react";
+import {
+  Check,
+  ChevronLeft,
+  ChevronsUpDown,
+  CircleCheck,
+  Plus,
+} from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -22,37 +28,35 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import LeftBar from "@/components/leftBar";
 
-
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
 export default function AddProduct() {
-  // const [open, setOpen] = React.useState(false);
-  // const [value, setValue] = React.useState("");
+  const [open, setOpen] = useState(false);
+
 
   const [name, setName] = useState("");
   const [addInfo, setAddInfo] = useState("");
   const [barCode, setBarCode] = useState("");
-  const [tag, setTag]=useState("");
+  const [price, setPrice] = useState("");
+  const [qty, setQty] = useState("");
+  const [category, setCategory] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [categories, setCategories] = useState<string[]>([]);
+  const [typeName, setTypeName] = useState("");
+  const [typeValue, setTypeValue] = useState("");
+  const [tag, setTag] = useState("");
+
+
+
+  const [selectedColors, setSelectedColors] = useState<string[]>([]);
+  const [selectedSizes, setSelectedSizes] = useState<string[]>([]);
+
+  // Function to add a new category to the list
+  const addCategory = () => {
+    // Ensure category is not empty or a duplicate
+    if (category && !categories.includes(category)) {
+      setCategories((prevCategories) => [...prevCategories, category]);
+      setCategory(""); // Clear input after adding
+    }
+  };
 
   const createProduct = async () => {
     try {
@@ -62,7 +66,12 @@ export default function AddProduct() {
           name: name,
           addInformation: addInfo,
           barCode: barCode,
-          tag:tag,
+          price: price,
+          qty: qty,
+          category: selectedCategory,
+          typeName: typeName,
+          typeValue: typeValue,
+          tag: tag,
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
@@ -72,6 +81,12 @@ export default function AddProduct() {
         setName("");
         setAddInfo("");
         setBarCode("");
+        setPrice("");
+        setQty("");
+        setCategory("");
+        setCategories([]);
+        setTypeName("");
+        setTypeValue("");
         setTag("");
       });
     } catch (error) {
@@ -79,6 +94,7 @@ export default function AddProduct() {
       console.error("error happened during creating the product", error);
     }
   };
+
   return (
     <div className="flex">
       <LeftBar />
@@ -86,7 +102,9 @@ export default function AddProduct() {
         <div className="bg-[#FFFFFF] ">
           <div className="py-2 flex gap-4 px-4">
             <ChevronLeft color="black" />
-            <p className="text-[#121316] font-normal text-base">Бүтээгдэхүүн нэмэх</p>
+            <p className="text-[#121316] font-normal text-base">
+              Бүтээгдэхүүн нэмэх
+            </p>
           </div>
         </div>
         <div className="py-8 px-8 flex gap-6 ">
@@ -98,8 +116,7 @@ export default function AddProduct() {
                     Бүтээгдэхүүний нэр
                   </Label>
                   <Input
-                    type="email"
-                    id="email"
+                    type="text"
                     placeholder="Нэр"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
@@ -124,8 +141,7 @@ export default function AddProduct() {
                     Барааны код
                   </Label>
                   <Input
-                    type="email"
-                    id="email"
+                    type="text"
                     placeholder="#12345678"
                     value={barCode}
                     onChange={(e) => setBarCode(e.target.value)}
@@ -150,10 +166,11 @@ export default function AddProduct() {
                     Үндсэн үнэ
                   </Label>
                   <Input
-                    type="email"
-                    id="email"
+                    type="text"
                     placeholder="Үндсэн үнэ"
-                    className="bg-[#F7F7F8] border-[#D6D8DB] h-[56px] w-[250px]"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    className="bg-[#F7F7F8] border-[#D6D8DB] h-[56px] w-[250px] text-black"
                   />
                 </div>
 
@@ -162,135 +179,108 @@ export default function AddProduct() {
                     Үлдэгдэл тоо ширхэг
                   </Label>
                   <Input
-                    type="email"
-                    id="email"
+                    type="text"
                     placeholder="Үлдэгдэл тоо ширхэг"
-                    className="bg-[#F7F7F8] border-[#D6D8DB] h-[56px] w-[250px]"
+                    value={qty}
+                    onChange={(e) => setQty(e.target.value)}
+                    className="bg-[#F7F7F8] border-[#D6D8DB] h-[56px] w-[250px] text-black"
                   />
                 </div>
               </div>
             </div>
           </div>
+
           <div className="flex flex-col gap-6">
             <div className="bg-[#FFFFFF] rounded-xl h-[232px]">
               <div className="py-6 px-6">
                 <div className="flex flex-col gap-2">
                   <Label className="text-[#121316] font-semibold text-sm">
-                    Ангилал
+                    Ангилал нэмэх
                   </Label>
-                  <Input
-                    type="email"
-                    id="email"
-                    placeholder="нэмэх"
-                    className="bg-[#F7F7F8] border-[#D6D8DB] w-[515px] h-[56px]"
-                  />
+                  <div className="relative ">
+                    <Input
+                      type="text"
+                      placeholder="нэмэх"
+                      value={category}
+                      onChange={(e) => setCategory(e.target.value)}
+                      className="bg-[#F7F7F8] border-[#D6D8DB] w-[515px] h-[56px] text-black"
+                    />
+                    <CircleCheck
+                      onClick={addCategory}
+                      className="absolute top-4 right-3 cursor-pointer"
+                      color="black"
+                    />
+                  </div>
 
                   <Label className="text-[#121316] font-semibold text-sm">
                     Ангилал
                   </Label>
-                  {/* <Popover open={open} onOpenChange={setOpen}>
-                  <PopoverTrigger asChild>
-                    <Button
-                      role="combobox"
-                      aria-expanded={open}
-                      className="w-[515px] h-[56px] bg-[#F7F7F8] hover:bg-[#F7F7F8] text-black justify-between"
-                    >
-                      {value
-                        ? frameworks.find(
-                            (framework) => framework.value === value
-                          )?.label
-                        : "сонгох"}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command>
-                      <CommandInput placeholder="Ангилал хайх..." />
-                      <CommandList>
-                        <CommandEmpty>Ангилал олдсонгүй.</CommandEmpty>
-                        <CommandGroup>
-                          {frameworks.map((framework) => (
-                            <CommandItem
-                              key={framework.value}
-                              value={framework.value}
-                              onSelect={(currentValue) => {
-                                setValue(
-                                  currentValue === value ? "" : currentValue
-                                );
-                                setOpen(false);
-                              }}
-                            >
-                              <Check
-                                className={cn(
-                                  "mr-2 h-4 w-4",
-                                  value === framework.value
-                                    ? "opacity-100"
-                                    : "opacity-0"
-                                )}
-                              />
-                              {framework.label}
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </CommandList>
-                    </Command>
-                  </PopoverContent>
-                </Popover> */}
-                </div>
-              </div>
-            </div>
-
-            <div className="bg-[#FFFFFF] rounded-xl h-[228px]">
-              <div className="pt-6 pl-6">
-                <h1 className="text-[#000000] text-lg font-semibold">Төрөл</h1>
-                <div className="flex pt-6 gap-6 items-center ">
-                  <h2 className="text-[#000000] font-normal text-sm ">Өнгө</h2>
-                  <Button className=" bg-[#ECEDF0] rounded-full py-1 px-1 w-6 h-6 ">
-                    <Plus color="black" size={20} />
-                  </Button>
-                </div>
-                <div className="flex gap-6 items-center pt-2">
-                  <h2 className="text-[#000000] font-normal text-sm ">
-                    Хэмжээ
-                  </h2>
-                  <Button className=" bg-[#ECEDF0] rounded-full py-1 px-1 w-6 h-6 ">
-                    <Plus color="black" size={20} />
-                  </Button>
-                </div>
-
-                <div className="py-6">
-                  <Popover>
+                  <Popover open={open} onOpenChange={setOpen}>
                     <PopoverTrigger asChild>
-                      <Button variant="outline" className="text-[#121316]">
-                        Төрөл нэмэх
+                      <Button
+                        role="combobox"
+                        aria-expanded={open}
+                        className="w-[515px] h-[56px] bg-[#F7F7F8] hover:bg-[#F7F7F8] text-black justify-between"
+                      >
+                        <span className="truncate">
+                          {selectedCategory
+                            ? selectedCategory
+                            : "Ангилал сонгох"}
+                        </span>
+                        <ChevronsUpDown className="ml-2 h-5 w-5 shrink-0 opacity-50" />
                       </Button>
                     </PopoverTrigger>
-                    <PopoverContent className="w-80">
-                      <div className="grid gap-4">
-                        <div className="space-y-2">
-                          <h4 className="font-medium leading-none">
-                            Dimensions
-                          </h4>
-                          <p className="text-sm text-muted-foreground">
-                            Set the dimensions for the layer.
-                          </p>
-                        </div>
-                        <div className="grid gap-2">
-                          <div className="grid grid-cols-3 items-center gap-4">
-                            <Label htmlFor="width">Width</Label>
-                            <Input
-                              id="width"
-                              defaultValue="100%"
-                              className="col-span-2 h-8"
-                            />
-                          </div>
-                        </div>
-                      </div>
+                    <PopoverContent className="w-[515px] p-0">
+                      <Command>
+                        <CommandInput placeholder="Ангилал сонгох..." />
+                        <CommandList>
+                          <CommandEmpty>Ангилал олдсонгүй.</CommandEmpty>
+                          <CommandGroup>
+                            {categories.map((cat) => (
+                              <CommandItem
+                                key={cat}
+                                onSelect={() => {
+                                  setSelectedCategory(cat);
+                                  setOpen(false);
+                                }}
+                              >
+                                <Check
+                                  className={cn(
+                                    "mr-2 h-4 w-4",
+                                    selectedCategory === cat
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                                {cat}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
                     </PopoverContent>
                   </Popover>
                 </div>
               </div>
             </div>
+
+
+            <div className="bg-[#FFFFFF] rounded-xl ">
+              <div className="py-6 px-6">
+                <div className="text-[#000000] font-semibold text-lg">Төрөл</div>
+                <div className="flex">
+                  <div className="text-black">Өнгө</div>
+                  <Button className="bg-[#ECEDF0] w-[32px] h-[32px] rounded-full"><Plus /></Button>
+                </div>
+
+                <div className="flex">
+                  <div className="text-black">Хэмжээ</div>
+                  <Button className="bg-[#ECEDF0] w-[32px] h-[32px] rounded-full"><Plus /></Button>
+                </div>
+
+              </div>
+            </div>
+
 
             <div className="bg-[#FFFFFF] rounded-xl h-[195px]">
               <div className="pt-6 pl-6">
@@ -300,7 +290,7 @@ export default function AddProduct() {
                   id="email"
                   placeholder="Таг нэмэх..."
                   value={tag}
-                  onChange={(e)=>setTag(e.target.value)}
+                  onChange={(e) => setTag(e.target.value)}
                   className="bg-[#F7F7F8] border-[#D6D8DB] w-[515px] h-[56px] pt-2"
                 />
                 <p className="text-[#5E6166] pt-2">
@@ -320,6 +310,12 @@ export default function AddProduct() {
           >
             Нийтлэх
           </Button>
+            
+
+          
+
+            
+          
         </div>
       </div>
     </div>
