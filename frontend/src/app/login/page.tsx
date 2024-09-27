cf41c0d8a5a1f7b88a1b0dc714e4c554370cbbad
Odonkhuu Munkhzul <odonkhuumunkhzul@Odonkhuus-MacBook-Pro.local>
2024-09-27 12:40:59 +0800

    add formik at register

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,106 +1,86 @@
 "use client";
-
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { FormikErrors, useFormik } from "formik";
 import Link from "next/link";
-import { useState } from "react";
 
-
+interface FormValues {
+    email: string,
+    password: string,
+    rePassword: string
+}
 export default function LoginPage() {
-    const [name, setName] = useState("true");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [emailError, setEmailError] = useState("");
-    const [passwordError, setPasswordError] = useState("");
-    const handleRegister = async () => { 
-        try{
-            const register=await fetch("http://localhost:4000/register",{
-                method: "Post",
-                body:JSON.stringify({
-                    name:name,
-                    email:email,
-    
-                    password:password,
-                }),
-                headers:{
-                    "Content-type":"appplication/json; charset=UTF-8",
-                },
-            }).then(()=>{
-                
-            })
-            console.log('Data created:', register);
-        }catch(error){
-             console.error('Error:', error);
-
+    const initialValues = {
+        email: '',
+        password: '',
+        repassword: ''
+    }
+    const formik = useFormik({
+        initialValues,
+        onSubmit: (values) => {
+            alert(`email:${values.email}, password: ${values.password}, repassword: ${values.repassword}`)
+        },
+        validate: (values) => {
+            const error: FormikErrors<FormValues> = {};
+            if (!values.email) {
+                error.email = "И-Мэйл ээ оруулна уу?"
+            }
+            if (!values.password) {
+                error.password = "Нууц үгээ оруулна уу?"
+            }
+            if (!values.repassword) {
+                error.rePassword = "Нууц үгээ давтаж оруулна уу?"
+            }
+            return error;
         }
-    };
-    const onButtonClick = () => {
-        setEmailError("");
-        setPasswordError("");
-
-        if ("" === email) {
-            setEmailError("Please enter your email");
-            return;
-        }
-
-        if ("" === password) {
-            setPasswordError("Please enter a password");
-            return;
-        }
-        if (password.length < 7) {
-            setPasswordError("password must be 8 character or longer");
-            return;
-        }
-
-        if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-            setEmailError("please enter a valid email address");
-            return;
-        }
-    };
+    })
 
 
     return (
-        <div className="container mx-auto login-box flex mx-auto align-items-center w-[334px] mt-20">
-            <div className="w-[334px] h-[446px]">
-                <h1 className="font-bold mb-5 text-center text-align-center">Нэвтрэх</h1>
-                <Input
-                    type="email"
-                    placeholder="И-мэйл хаяг"
-                    className="rounded-sm w-full mt-5 mb-5"
-                    onChange={(e) => setEmail(e.target.value)}
-                ></Input>
+        <div className="container mx-auto login-box flex mx-auto align-items-center w-[334px] mt-20 my-auto">
+            <form onSubmit={formik.handleSubmit}>
+                <div className="w-[334px] h-[446px]">
+                    <h1 className="font-bold mb-5 text-center text-align-center">Нэвтрэх</h1>
+                    <Input
+                        type="text"
+                        placeholder="И-мэйл хаяг"
+                        className="rounded-sm w-full my-5   "
+                        id="email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.errors.email ? <span className="text-red-500 text-sm text-start my-2">Нэрээ оруулна уу!</span> : null}
+                    <Input
+                        type="password"
+                        placeholder="Нууц үг"
+                        className="rounded-sm w-full mb-3"
+                    />
+                    {formik.errors.password ? <span className="text-red-500 text-sm text-start">Нууц үгээ оруулна уу</span> : null}
+                    <Input
+                        type="password1"
+                        placeholder="Нууц үг давтах"
+                        className="rounded-sm w-full mb-3"
+                    />
+                    {formik.errors.repassword ? <span className="text-red-500 text-sm text-start my-2">Нууц үгээ давтаж оруулна уу</span> : null}
 
-                <Input
-                    type="password"
-                    placeholder="Нууц үг"
-                    className="rounded-sm w-full mb-5"
-                ></Input>
-                <Input
-                    type="password1"
-                    placeholder="Нууц үг давтах"
-                    className="rounded-sm w-full mb-5"
-                ></Input>
-                <div className="text-center">
-                    <Link href="/forgotPass" className="text-slate-300 text-align-center text-center" type="forgetpassword" >Нууц үг мартсан</Link>
-                </div>
-                <Button asChild variant="def2" className="rounded-sm">
+                    <div className="text-center">
+                        <Link href="/forgotPass" className="text-slate-300 text-align-center text-center" type="forgetpassword" >Нууц үг мартсан</Link>
+                    </div>
+
+
                     <Link
-                        href="/login"
-                        className="bg-blue-500 mt-8 w-full"
-                        type="button"
-                    //   value="submit"
-                    >
-                        Нэвтрэх
+                        href="/login" type="submit">
+                        <Button variant="def2" className="bg-blue-500 mt-8 w-full" type="submit">Нэвтрэх</Button>
                     </Link>
-                </Button>
-                <Button asChild variant="def3">
-                    <Link href="/register" className="bg-blue-500 w-full mt-5">
-                        Үүсгэх
-                    </Link>
-                </Button>
-            </div>
-        </div>
+
+                    <Button asChild variant="def3">
+                        <Link href="/register" className="bg-blue-500 w-full mt-5">
+                            Үүсгэх
+                        </Link>
+                    </Button>
+                </div>
+            </form >
+        </div >
     );
 
 }
