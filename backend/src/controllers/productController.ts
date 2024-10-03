c4a3d6c38a3c74f2a0e628dd5581774eb8dd2073
satsuralala <satsuraltumurbat@gmail.com>
2024-10-03 19:08:04 +0800

    zassan

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -13,16 +13,7 @@ const getProducts = async (req: Request, res: Response) => {
   }
 };
 
-import { productModel } from "../models/product"
-import {Request, Response} from "express";
-const getProducts=async(req:Request, res:Response)=>{
-    try{
-        const products=await productModel.find();
-        res.send(products);
-    }catch(error){
-        res.status(400).json({error:"error happened during finding the products"})
-    }
-}
+
 
 const getProduct=async(req:Request, res:Response)=>{
 
@@ -90,91 +81,9 @@ const updateProduct=async(req:Request, res:Response)=>{
 
 
 
-const getProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await productModel.findById(id);
-    res.send(product);
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error: "error happened during finding the product" });
-  }
-};
 
-const createProduct = async (req: Request, res: Response) => {
-   
-  try {
-    const {
-      name,
-      addInformation,
-      barCode,
-      uploadedPhotos,
-      price,
-      qty,
-      category,
-      selectedColors,
-      selectedSizes,
-      tag,
-      createdAt,
-    } = req.body;
-    const product = await productModel.create({
-      name,
-      addInformation,
-      barCode,
-      uploadedPhotos,
-      price,
-      qty,
-      category,
-      selectedColors,
-      selectedSizes,
-      tag,
-      createdAt: new Date(),
-    });
-    res.status(201).json({ message: "Successfully created the product" });
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error: "Error occurred while creating the product" });
-  }
-};
 
-const updateProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const {
-      name,
-      addInformation,
-      barCode,
-      uploadedPhotos,
-      price,
-      qty,
-      category,
-      selectedColors,
-      selectedSizes,
-      tag,
-      createdAt,
-    } = req.body;
-    const product = await productModel.findByIdAndUpdate(id, {
-      name,
-      addInformation,
-      barCode,
-      uploadedPhotos,
-      price,
-      qty,
-      category,
-      selectedColors,
-      selectedSizes,
-      tag,
-      createdAt,
-    });
-    res.status(200).json({ message: "Successfully updated the product" });
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error: "error happened during updating the product" });
-  }
-};
+
 
 const deleteProduct = async (req: Request, res: Response) => {
   try {
