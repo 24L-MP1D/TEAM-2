af7ced679d9535af80d3048ae0987da6bde1e4ca
satsuralala <satsuraltumurbat@gmail.com>
2024-10-08 10:53:40 +0800

    solveConflict

--- backend/src/models/cart.ts
+++ backend/src/models/cart.ts
@@ -1,24 +1,11 @@
 import {model, Schema} from "mongoose";
 
 const cartSchema= new Schema({
-<<<<<<< HEAD
   userId: String,
   productId:String,
   selectedCount: Number,
   // selectedColors: [{ type: String }],
   selectedSizes: [{type:String}],
-=======
-    name: String,
-  addInformation:String,
-  barCode:String,
-  uploadedPhotos:[{type:String}],
-  price: Number,
-  qty: Number,
-  category: String,
-  selectedColors: [{ type: String }],
-  selectedSizes: [{type:String}],
-  tag: String,
->>>>>>> d260c444e8e1dc8e4f0862d2e7f48e737096dd6b
   createdAt: Date,
   updatedAt: Date,
 })
--- backend/src/routes/cartRoutes.ts
+++ backend/src/routes/cartRoutes.ts
@@ -1,5 +1,4 @@
 import express from "express";
-<<<<<<< HEAD
 import { getCart, createCart } from "..//controllers/CartController";
 
 const cartRouter = express.Router();
@@ -8,13 +7,3 @@ cartRouter
     .get("cart/", getCart)
     .post("/productdetails", createCart)
 export { cartRouter };
-=======
-import {getCart, createCart} from "..//controllers/CartController";
-
-const cartRouter = express.Router();
-cartRouter 
-.get("/cart", getCart)
-.get("cart/", getCart)
-.post("/cart", createCart)
-export {cartRouter};
->>>>>>> d260c444e8e1dc8e4f0862d2e7f48e737096dd6b
